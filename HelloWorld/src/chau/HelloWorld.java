e72db43a0c83689486314a1303da4023d22d4371
Wayne Chau <waynechichau2000@hotmail.com>
2017-02-21 11:23:42 -0500

    I am pushing the program HelloWorld.

---
+++ HelloWorld/src/chau/HelloWorld.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package chau;
+
+/**
+ * HelloWorld.java
+ * This program prints HelloWorld! to the console
+ * February 21 2016
+ * @author Wayne Chau
+ */
+public class HelloWorld {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello,World!");
+
+	}
+
+}
