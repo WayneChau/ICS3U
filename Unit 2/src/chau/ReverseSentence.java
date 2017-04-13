3b79b70e175770b92feb0f531f40d7eb3c0441b5
Wayne Chau <waynechichau2000@hotmail.com>
2017-04-13 11:17:57 -0400

    Final Push of Reverse Sentence

---
+++ Unit 2/src/chau/ReverseSentence.java
@@ -0,0 +1,36 @@
+package chau;
+
+import java.util.Scanner;
+
+/**
+ * ReverseSentence.java 
+ * April 13, 2017
+ * This program takes the sentence inputed by the user and prints it backwards. The word that is capitalized forward will be capitalized backwards in the same area capitalized.
+ * @author WayneChau 
+ */
+public class ReverseSentence {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String[] sentence = sc.nextLine().split(" ");
+		for (int i = 0; i < sentence.length; i++) {
+
+			for (int j = sentence[i].length() - 1; j >= 0; j--) {
+				if (((Character) (sentence[i].charAt(0))).toString().matches("[A-Z]")) {
+					if (j == sentence[i].length() - 1) {
+						System.out.print(Character.toUpperCase(sentence[i].charAt(j)));
+					} else {
+						System.out.print(Character.toLowerCase(sentence[i].charAt(j)));
+					}
+				} else {
+					System.out.print(sentence[i].charAt(j));
+				}
+				if(sentence[i].charAt(j) == '.' ){
+				
+					System.out.print('.');
+				}
+			}
+			System.out.print(" ");
+		}
+	}
+}
